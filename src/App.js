edeccb13967c00644881687c4587e9b0629cbd2f
driss292 <drisskaci@outlook.fr>
2022-07-22 16:43:57 +0200

    cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { commerce } from "./lib/commerce";
+import { Products, Navbar, Cart } from "./components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+        setProducts(data);
+    };
+
+    const fetchCart = async () => {
+        const cart = await commerce.cart.retrieve();
+        setCart(cart);
+    };
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+        setCart(item.cart);
+    };
+
+    useEffect(() => {
+        fetchProducts();
+        fetchCart();
+    }, []);
+    console.log(cart);
+
+    console.log(products);
+    return (
+        <Router>
+            <div>
+                <Navbar totalItems={cart.total_items} />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <Products
+                                products={products}
+                                onAddToCart={handleAddToCart}
+                            />
+                        }
+                    />
+                    <Route path="/cart" element={<Cart cart={cart} />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import Product from "./Product/Product";
+import useStyles from "./styles";
+
+// const products = [
+//     {
+//         id: 1,
+//         name: "Shoes",
+//         description: "Running shoes.",
+//         price: "€5",
+//         image: "https://images.unsplash.com/photo-1592503254549-d83d24a4dfab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80",
+//     },
+//     {
+//         id: 2,
+//         name: "Macbook",
+//         description: "Apple macbook.",
+//         price: "€10",
+//         image: "https://images.unsplash.com/photo-1592503254549-d83d24a4dfab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80",
+//     },
+// ];
+
+const Products = ({ products, onAddToCart }) => {
+    const classes = useStyles();
+    return (
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Grid container justifyContent="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} onAddToCart={onAddToCart} />
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    );
+};
+
+export default Products;
